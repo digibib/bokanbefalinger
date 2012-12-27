06c5393324b87bc493b3cebfe487bf88f1f75fc1
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2012-12-27 13:09:27 +0100

    search form routes to /search

--- routes/reviews.rb
+++ routes/reviews.rb
@@ -1,5 +1,11 @@
 # encoding: utf-8
 class BokanbefalingerApp < Sinatra::Application
+  post "/search" do
+    searchterms = request.params["search"]
+    @title = "Søk i anbefalinger: #{searchterms}"
+    erb :reviews
+  end
+
   get "/reviews" do
     @title  = "Siste anbefalinger"
     erb :reviews
