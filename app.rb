5840991b1f8a5d8fd4ed4732b6e8ac206298ff7f
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2012-12-30 08:52:43 +0100

    Adds get and set cache helpers

--- app.rb
+++ app.rb
@@ -7,8 +7,11 @@ BASE_URI="http://data.deichman.no/bookreviews/"
 
 class BokanbefalingerApp < Sinatra::Application
   register Sinatra::Async
-  REDIS = Redis.new #(:driver => :synchrony)
   enable :sessions
+
+  # Enable caching here:
+  set :caching, true
+  CACHE = Redis.new      # Assumes Redis is running on http://localhost:6379
 
   configure :production do
     #set :clean_trace, true
@@ -23,6 +26,25 @@ class BokanbefalingerApp < Sinatra::Application
   helpers do
     include Rack::Utils
     alias_method :h, :escape_html
+
+    def get_cache(key)
+      return nil unless settings.caching
+      begin
+        cached = CACHE.get key
+      rescue Redis::CannotConnectError
+        puts "DEBUG: Redis not available. Cannot read from cache."
+      end
+      cached
+    end
+
+    def set_cache(key, value)
+      return nil unless settings.caching
+      begin
+        CACHE.set key, value
+      rescue Redis::CannotConnectError
+        puts "DEBUG: Redis not available. Cannot read from cache."
+      end
+    end
 
     def create_uri(path)
       BASE_URI+path.join("")
