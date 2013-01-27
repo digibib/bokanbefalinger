0e2ebc383c75246e8bf27478503315c1eb6a4115
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-01-27 01:47:35 +0100

    populate omtalte personer og subjects dropdowns

--- models/list.rb
+++ models/list.rb
@@ -6,8 +6,40 @@ class List
 
   @@conn = Faraday.new(:url => API)
 
-  def self.populate_dropdowns(criteria)
+  def self.populate_dropdowns()
+    subjects, persons = Cache.get("subjects"), Cache.get("persons")
+    return [eval(subjects), eval(persons)] if persons and subjects
+
+    query = QUERY.select(:subject_id, :subject_label, :person_id, :person_label)
+
+    query.distinct.from(BOOKGRAPH)
+    query.where([:work, RDF::FABIO.hasManifestation, :book],
+                [:book, RDF::REV.hasReview, :review],
+                [:book, RDF::DC.subject, :subject_id],
+                [:subject_id, RDF::SKOS.prefLabel, :subject_label ],
+                [:book, RDF::DC.subject, :person_id],
+                [:person_id, RDF::FOAF.name, :person_label])
+
+    puts query
+
+    result = REPO.select(query)
+    return [] if result.empty?
+
+    persons,subjects = {}, {}
+    result.each do |s|
+      persons[s[:person_id].to_s] = s[:person_label].to_s
+      subjects[s[:subject_id].to_s] = s[:subject_label].to_s
+    end
+    puts "Setting persons and subjects cache"
+    Cache.set "subjects", subjects
+    Cache.set "persons", persons
+
+    return [subjects, persons]
+  end
+
+  def self.repopulate_dropdowns(criteria)
     # criteria {:authors => ["fred", "hans"], :persons => [uri1, uri2] etc..}
+
   end
 
   def self.get(authors, subjects, persons)
@@ -15,6 +47,7 @@ class List
 
     authors_regex = authors.join("|") unless authors.empty?
 
+    # both subjecs & personss are dct:subject of book
     subj = subjects + persons
 
     query = QUERY.select(:review)
