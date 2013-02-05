f3a97cf7e8e5d6e8ddf499965d3ca5775844797a
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-02-05 11:34:45 +0100

    authenticate user via API

--- routes/user.rb
+++ routes/user.rb
@@ -1,11 +1,31 @@
 # encoding: utf-8
 class BokanbefalingerApp < Sinatra::Application
+  @@conn = Faraday.new(:url => "http://datatest.deichman.no/api/users/authenticate")
+
   post "/login" do
     unless request.params["username"].empty?||request.params["password"].empty?
-      # Gjør autentisering mot protected-graph i virtuoso her
+      puts "Authenticate user via API"
+      begin
+        resp = @@conn.post do |req|
+          req.body = {:username => params["username"],
+                      :password => params["password"]}.to_json
+        end
+      rescue Faraday::Error::TimeoutError, Faraday::Error::ConnectionFailed
+        return [nil, "Forespørsel til eksternt API(#{Settings::API}) brukte for lang tid å svare"]
+      end
 
-      session[:user] = request.params["username"]
-      redirect params["take_me_back"]
+      res = JSON.parse(resp.body)
+      puts res
+      if res["authenticated"]
+        puts "authenticated"
+        session[:user] = request.params["username"]
+        redirect params["take_me_back"]
+      else
+        puts "not authenticated"
+        session[:user] = session[:pass] = nil
+        redirect params["take_me_back"] if params["take_me_back"]
+        redirect '/'
+      end
     end
 
     redirect params["take_me_back"] if params["take_me_back"]
