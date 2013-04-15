dd69281ec0bacc133432e3304cf771cf1e3b9ad5
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-04-15 06:10:07 +0200

    gjør det mulig å abbonere på søk, via rss

--- routes/feed.rb
+++ routes/feed.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+require "time"
 
 class BokanbefalingerApp < Sinatra::Application
 
@@ -8,7 +9,26 @@ class BokanbefalingerApp < Sinatra::Application
     if list_params.empty?
       @error_message, @result = Review.get_latest(10, 0, 'issued', 'desc')
     else
-      uris = List.get_feed(request.url)
+      if list_params["work"]
+        @error_message, work = Work.get(list_params["work"].first)
+        # order reviews in work hash by issued
+        work["reviews"] = work["reviews"].sort_by { |k,v| Time.parse(k["issued"]).to_i }.reverse
+        uris = work["reviews"].collect { |r| r["uri"]}
+      elsif list_params["isbn"]
+        @error_message, work = Work.by_isbn(list_params["isbn"].first)
+        # remove unpublished reviews
+        work["reviews"].reject! { |r| r["published"] == false}
+        work["reviews"] = work["reviews"].sort_by { |k,v| Time.parse(k["issued"]).to_i }.reverse
+        uris = work["reviews"].collect { |r| r["uri"]}
+      elsif list_params["reviewer"]
+        @error_message, reviews = Review.by_reviewer(list_params["reviewer"].first)
+        uris = reviews.collect { |r| r["reviews"].first["uri"] }
+      elsif list_params["source"]
+        @error_message, reviews = Review.by_source(list_params["source"].first)
+        uris = reviews.collect { |r| r["reviews"].first["uri"] unless r["reviews"].first["published"] == false}
+      else
+        uris = List.get_feed(request.url)
+      end
       @reviews = []
       uris[0..10].each do |uri|
         _, r = Review.get(uri)
@@ -20,7 +40,8 @@ class BokanbefalingerApp < Sinatra::Application
 
     if @error_message
       @title = "Feil"
-      erb :error
+      erb :error # TODO annen tekstfeilmelding? + set statuskode 400/500 eller no:
+                 # (500) generering av RSS/atom feilet.
     else
       builder :feed, :locals => {:result => @result, :format => request.accept, :url => request.url, :title => params["title"]}
     end
