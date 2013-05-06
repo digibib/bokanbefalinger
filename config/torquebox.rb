3ed12d9d22e3de4ec2a26f076823af675cb9d490
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-05-06 10:35:51 +0200

    config docs

--- config/torquebox.rb
+++ config/torquebox.rb
@@ -1,16 +1,26 @@
+# -----------------------------------------------------------------------------
+# torquebox.rb - torquebox settings
+# -----------------------------------------------------------------------------
+# Edit this file to set scheduled jobs, queues and long running processess
+
 TorqueBox.configure do
+
   web do
-    context "/"
+    context "/" # where to mount application
   end
+
+
+  # Message queues
 
   options_for :messaging, :default_message_encoding => :edn
 
-  # Message queues
   queue '/queues/cache' do
     processor CacheProcessor
   end
 
+
   # Scheduled jobs (cron)
+
   job LatestJob do
     cron "0 0/15 * * * ?"
   end
