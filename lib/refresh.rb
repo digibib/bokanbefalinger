72868e750f92446c4f006a853240e7d0237e5385
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-05-13 08:40:32 +0200

    use lib/sparql

--- lib/refresh.rb
+++ lib/refresh.rb
@@ -14,6 +14,7 @@ require "torquebox-messaging"
 
 require_relative "../config/settings"
 require_relative "../models/init"
+require_relative "../lib/sparql"
 require_relative "cache"
 
 module Refresh
@@ -24,20 +25,9 @@ module Refresh
   QUEUE = TorqueBox::Messaging::Queue.new('/queues/cache')
 
   def latest
-    old = Cache.get("reviews:latest")
+    old = Cache.get("reviews:latest") { [] }
 
-    q = QUERY.select(:review)
-    q.distinct
-    q.from(BOOKGRAPH)
-    q.from_named(REVIEWGRAPH)
-    q.where([:work, RDF::FABIO.hasManifestation, :book],
-                [:book, RDF::REV.hasReview, :review])
-    q.where([:review, RDF::DC.issued, :issued, :context => REVIEWGRAPH])
-    q.order_by("DESC(?issued)")
-    q.limit(100)
-
-    res = REPO.select(q)
-    cache = res.bindings[:review]
+    cache = SPARQL::Reviews.latest(0,100)
     Cache.set("reviews:latest", cache, :various)
 
     # Check if any new reviews
@@ -143,195 +133,28 @@ module Refresh
   end
 
   def dropdowns
-    # 1. persons
-    q = QUERY.select(:subject, :subject_label)
-    q.distinct
-    q.from(BOOKGRAPH)
-    q.where([:work, RDF::FABIO.hasManifestation, :book],
-            [:book, RDF::REV.hasReview, :review],
-            [:book, RDF::DC.subject, :subject_narrower],
-            [:subject, RDF::SKOS.narrower, :subject_narrower],
-            [:subject, RDF::SKOS.prefLabel, :subject_label])
-    res = REPO.select(q)
-    subjects = {}
-
-    res.each do |s|
-      subjects[s[:subject].to_s] = s[:subject_label].to_s
-    end
-
-    # 2. subjects
-    Cache.set("dropdown:subjects", subjects, :dropdowns)
-
-    q = QUERY.select(:person, :person_name, :lifespan)
-    q.distinct
-    q.from(BOOKGRAPH)
-    q.where([:work, RDF::FABIO.hasManifestation, :book],
-            [:book, RDF::REV.hasReview, :review],
-            [:book, RDF::DC.subject, :person],
-            [:person, RDF::FOAF.name, :person_name],
-            [:person, RDF::URI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), RDF::FOAF.Person])
-    q.optional([:person, RDF::DEICHMAN.lifespan, :lifespan])
-    res = REPO.select(q)
-    persons = {}
-
-    res.each do |s|
-      if s[:lifespan]
-        lifespan = " (#{s[:lifespan]})"
-      else
-        lifespan = ""
-      end
-      persons[s[:person].to_s] = s[:person_name].to_s + lifespan
-    end
+    persons       = SPARQL::Dropdown.persons
+    subjects      = SPARQL::Dropdown.subjects
+    genres        = SPARQL::Dropdown.genres
+    languages     = SPARQL::Dropdown.languages
+    authors       = SPARQL::Dropdown.authors
+    formats       = SPARQL::Dropdown.formats
+    nationalities = SPARQL::Dropdown.nationalities
+    titles        = SPARQL::Dropdown.titles
+    reviewers     = SPARQL::Dropdown.reviewers
+    sources       = SPARQL::Dropdown.sources
 
     Cache.set("dropdown:persons", persons, :dropdowns)
-
-    # 3. genres
-    q = QUERY.select(:genre, :genre_label)
-    q.distinct
-    q.from(BOOKGRAPH)
-    q.where([:work, RDF::FABIO.hasManifestation, :book],
-            [:book, RDF::REV.hasReview, :review],
-            [:book, RDF::DBO.literaryGenre, :narrower],
-            [:narrower, RDF::SKOS.broader, :genre],
-            [:genre, RDF::RDFS.label, :genre_label])
-    res = REPO.select(q)
-    genres = {}
-
-    res.each do |s|
-      genres[s[:genre].to_s] = s[:genre_label].to_s
-    end
-
+    Cache.set("dropdown:subjects", subjects, :dropdowns)
     Cache.set("dropdown:genres", genres, :dropdowns)
-
-    # 4. languages
-    q = QUERY.select(:language, :language_label)
-    q.distinct
-    q.from(BOOKGRAPH)
-    q.where([:work, RDF::FABIO.hasManifestation, :book],
-            [:book, RDF::REV.hasReview, :review],
-            [:book, RDF::DC.language, :language],
-            [:language, RDF::RDFS.label, :language_label])
-    res = REPO.select(q)
-    languages = {}
-
-    res.each do |s|
-      languages[s[:language].to_s] = s[:language_label].to_s
-    end
-
     Cache.set("dropdown:languages", languages, :dropdowns)
-
-    # 5. authors
-    q = QUERY.select(:author, :author_name)
-    q.distinct
-    q.from(BOOKGRAPH)
-    q.where([:work, RDF::FABIO.hasManifestation, :book],
-            [:work, RDF::DC.creator, :author],
-            [:author, RDF::FOAF.name, :author_name],
-            [:book, RDF::REV.hasReview, :review])
-    res = REPO.select(q)
-    authors = {}
-
-    res.each do |s|
-      authors[s[:author].to_s] = s[:author_name].to_s
-    end
-
     Cache.set("dropdown:authors", authors, :dropdowns)
-
-    # 6. formats
-    q = QUERY.select(:format, :format_label)
-    q.distinct
-    q.from(BOOKGRAPH)
-    q.where([:work, RDF::FABIO.hasManifestation, :book],
-            [:book, RDF::REV.hasReview, :review],
-            [:book, RDF::DEICHMAN.literaryFormat, :format],
-            [:format, RDF::RDFS.label, :format_label])
-    res = REPO.select(q)
-    formats = {}
-
-    res.each do |s|
-      formats[s[:format].to_s] = s[:format_label].to_s
-    end
-
     Cache.set("dropdown:formats", formats, :dropdowns)
-
-    # 7. nationalities
-    q = QUERY.select(:nationality, :nationality_label)
-    q.distinct
-    q.from(BOOKGRAPH)
-    q.where([:work, RDF::FABIO.hasManifestation, :book],
-            [:book, RDF::REV.hasReview, :review],
-            [:work, RDF::DC.creator, :creator],
-            [:creator, RDF::XFOAF.nationality, :nationality],
-            [:nationality, RDF::RDFS.label, :nationality_label])
-    res = REPO.select(q)
-    nationalities = {}
-
-    res.each do |s|
-      nationalities[s[:nationality].to_s] = s[:nationality_label].to_s
-    end
-
     Cache.set("dropdown:nationalities", nationalities, :dropdowns)
-
-    # 8. titles
-    q = QUERY.select(:work, :title)
-    q.sample(:original_title)
-    q.distinct
-    q.from(BOOKGRAPH)
-    q.where([:work, RDF::FABIO.hasManifestation, :book],
-            [:work, RDF::DC.title, :original_title],
-            [:book, RDF::REV.hasReview, :review],
-            [:book, RDF::DC.title, :title])
-    res = REPO.select(q)
-    titles = {}
-
-    res.each do |s|
-      original_title = ""
-      original_title += " (#{s[:original_title]})" unless s[:title] == s[:original_title]
-      titles[s[:work].to_s] = s[:title].to_s + original_title
-    end
-
     Cache.set("dropdown:titles", titles, :dropdowns)
-
-    # 9. reviewers
-    q = QUERY.select(:reviewer, :reviewer_name)
-    q.distinct
-    q.from(BOOKGRAPH)
-    q.from_named(REVIEWGRAPH)
-    q.from_named(APIGRAPH)
-    q.where([:work, RDF::FABIO.hasManifestation, :book],
-            [:book, RDF::REV.hasReview, :review])
-    q.where([:review, RDF::REV.reviewer, :reviewer, :context => REVIEWGRAPH])
-    q.where([:reviewer, RDF::FOAF.name, :reviewer_name, :context => APIGRAPH])
-    res = REPO.select(q)
-    reviewers = {}
-
-    res.each do |s|
-      reviewers[s[:reviewer].to_s] = s[:reviewer_name].to_s
-    end
-
     Cache.set("dropdown:reviewers", reviewers, :dropdowns)
-
-    # 10. sources
-    q = QUERY.select(:source, :source_name, :dropdowns)
-    q.distinct
-    q.from(BOOKGRAPH)
-    q.from_named(REVIEWGRAPH)
-    q.from_named(APIGRAPH)
-    q.where([:work, RDF::FABIO.hasManifestation, :book],
-            [:book, RDF::REV.hasReview, :review])
-    q.where([:review, RDF::DC.source, :source, :context => REVIEWGRAPH])
-    q.where([:source, RDF::FOAF.name, :source_name, :context => APIGRAPH])
-
-    res = REPO.select(q)
-    sources = {}
-
-    res.each do |s|
-      sources[s[:source].to_s] = s[:source_name].to_s
-    end
-
     Cache.set("dropdown:sources", sources, :dropdowns)
 
-    # Done
     puts "Refreshed dropdown caches"
   end
 end
