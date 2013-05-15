7d1245618bd81b7fa58af7e3abd2be14eb7aeaf1
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-05-15 12:44:38 +0200

    add review to my list. make list sortable

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,9 @@
 /* Array.prototype.indexOf for IE (issue #26) */
 if(!Array.prototype.indexOf){Array.prototype.indexOf=function(b){var a=this.length,c=Number(arguments[1])||0;c=c<0?Math.ceil(c):Math.floor(c);if(c<0){c+=a}for(;c<a;c++){if(c in this&&this[c]===b){return c}}return -1}};
 
+/* HTML5 Sortable (http://farhadi.ir/projects/html5sortable)
+ * Released under the MIT license.
+ */(function(a){var b,c=a();a.fn.sortable=function(d){var e=String(d);return d=a.extend({connectWith:!1},d),this.each(function(){if(/^enable|disable|destroy$/.test(e)){var f=a(this).children(a(this).data("items")).attr("draggable",e=="enable");e=="destroy"&&f.add(this).removeData("connectWith items").off("dragstart.h5s dragend.h5s selectstart.h5s dragover.h5s dragenter.h5s drop.h5s");return}var g,h,f=a(this).children(d.items),i=a("<"+(/^ul|ol$/i.test(this.tagName)?"li":"div")+' class="sortable-placeholder">');f.find(d.handle).mousedown(function(){g=!0}).mouseup(function(){g=!1}),a(this).data("items",d.items),c=c.add(i),d.connectWith&&a(d.connectWith).add(this).data("connectWith",d.connectWith),f.attr("draggable","true").on("dragstart.h5s",function(c){if(d.handle&&!g)return!1;g=!1;var e=c.originalEvent.dataTransfer;e.effectAllowed="move",e.setData("Text","dummy"),h=(b=a(this)).addClass("sortable-dragging").index()}).on("dragend.h5s",function(){b.removeClass("sortable-dragging").show(),c.detach(),h!=b.index()&&f.parent().trigger("sortupdate",{item:b}),b=null}).not("a[href], img").on("selectstart.h5s",function(){return this.dragDrop&&this.dragDrop(),!1}).end().add([this,i]).on("dragover.h5s dragenter.h5s drop.h5s",function(e){return!f.is(b)&&d.connectWith!==a(b).parent().data("connectWith")?!0:e.type=="drop"?(e.stopPropagation(),c.filter(":visible").after(b),!1):(e.preventDefault(),e.originalEvent.dataTransfer.dropEffect="move",f.is(this)?(d.forcePlaceholderSize&&i.height(b.outerHeight()),b.hide(),a(this)[i.index()<a(this).index()?"after":"before"](i),c.not(i).detach()):!c.is(this)&&!a(this).children(d.items).length&&(c.detach(),a(this).append(i)),!1)})})}})(jQuery);
 // Vis skrivetips ved musover
 $.fn.ezpz_tooltip.positions.centerLeft = function(contentInfo, mouseX, mouseY, offset, targetInfo) {
   contentInfo['top'] = 480;
@@ -26,3 +29,40 @@ setInterval(function() {
   $("#loading").html(""+Array(i+1).join("."));
 }, 500);
 
+
+// My list logic
+$('document').ready(function() {
+
+	// Add to my list
+	$('button.pluss').on('click', function() {
+		var uri = $(this).parents('.liste-info').find('input.uri').val();
+		var title = $(this).parents('.liste-info').find('.liste-title a').text()
+
+		$('.single-list:first ol').append('<li><a href="' + uri +'">'+title+'</a><a class="remove">x</a></li>');
+
+		// refresh drag and sort
+		$('.sortable').sortable();
+	});
+
+	// remove review on click 'x'
+	$('.my-lists').on('click', '.remove', function() {
+		$(this).parents('li').remove();
+	});
+
+	// Show list when clicking on title or triangle
+	$('.single-list').on('click', '.triangle.close', function() {
+		$(this).removeClass("close").addClass("open");
+		$(this).next().next().slideUp();
+	});
+
+	$('.single-list').on('click', '.triangle.open', function() {
+		$('.liste-innhold').slideUp();
+		$('.close').removeClass("close").addClass("open");
+		$(this).removeClass("open").addClass("close");
+		$(this).next().next().slideDown();
+	});
+
+	$('.liste-tittel').on('click', function() {
+		$(this).next().click();
+	});
+});
